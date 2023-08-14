03675380f41c4780cf6173759fb42a4ce358d2ce
Eskrano <pommmper@gmail.com>
2023-08-14 18:04:06 +0300

    ✨ Add lodash.throttle . ✨ Add rename tab.

--- src/store/features/tabs/components/TabSettings.tsx
+++ src/store/features/tabs/components/TabSettings.tsx
@@ -1,6 +1,9 @@
+import { useAppDispatch, useAppSelector } from '@/store/hooks';
 import { Close, Settings } from '@mui/icons-material';
-import { IconButton } from '@mui/material';
-import React, { FC, useState } from 'react'
+import { IconButton, Input, Popover, TextField, Typography } from '@mui/material';
+import React, { FC, useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import { setTabTitle as reduxSetTabTitle } from '../tabsSlice';
+import throttle from "lodash.throttle"
 
 export type TabSettingsProps = {
     tabId: string | undefined | number;
@@ -8,19 +11,49 @@ export type TabSettingsProps = {
 
 export const TabSettings: FC<TabSettingsProps> = (props: TabSettingsProps) => {
 
-    const [showSettings, setShowSettings] = useState(false);
+    const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
 
-    if (!showSettings) {
-        return (
-            <>
-                <IconButton sx={{
-                    justifyContent: 'flex-end'
-                }} onClick={() => setShowSettings(true)}>
-                    <Settings />
-                </IconButton>
-            </>
-        )
-    }
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const dispatch = useAppDispatch();
+
+    const refInput = useRef<HTMLInputElement>(null);
+
+    const tabTitle = useAppSelector(
+        state => state.tabsSlice.tabs.find(tab => tab.id === props.tabId)?.title
+    );
+
+    useEffect(() => {
+        if (!tabTitle) return;
+        if (!refInput.current) return;
+
+        refInput.current.value = tabTitle;
+
+    }, [tabTitle]);
+
+    const throttledSetTabTitle = useMemo(
+        () => throttle((name: string) => {
+            dispatch(
+                reduxSetTabTitle({
+                    id: props.tabId,
+                    title: name
+                })
+            );
+        }, 100),
+        [dispatch, props.tabId]
+    );
+
+    const setTabTitle = useCallback(throttledSetTabTitle, [throttledSetTabTitle]);
+
+
+    const open = Boolean(anchorEl);
+    const id = open ? 'tab-settings-popover' : undefined;
 
     return (
         <>
@@ -28,9 +61,35 @@ export const TabSettings: FC<TabSettingsProps> = (props: TabSettingsProps) => {
                 sx={{
                     justifyContent: 'flex-end'
                 }}
-                onClick={() => setShowSettings(false)}>
-                <Close />
+                onClick={handleClick}>
+                {open ? <Close /> : <Settings />}
             </IconButton>
+            <Popover
+
+                id={id}
+                open={open}
+                anchorEl={anchorEl}
+                onClose={handleClose}
+
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'right',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                }}
+            >
+                <Typography sx={{ p: 2 }}>Tab settings.</Typography>
+
+                <TextField type='text' label="Change Tab Name" placeholder="Tab name" value={tabTitle || ''}
+                    onChange={(e) => {
+                        console.log(e.target.value)
+                        setTabTitle(e.target.value);
+                    }}
+                />
+
+            </Popover>
         </>
     )
 }
--- src/store/features/tabs/tabsSlice.ts
+++ src/store/features/tabs/tabsSlice.ts
@@ -1,3 +1,4 @@
+import store from "@/store";
 import { createSlice } from "@reduxjs/toolkit";
 import { produce } from "immer";
 
